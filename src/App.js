43dc21bfcd2552c204b3b6bea037672380a3abb3
Cédric Bois <cedric.bois@uca.fr>
2022-05-18 22:51:35 +0200

    ajout aide + liens soulignés

--- src/App.js
+++ src/App.js
@@ -10,8 +10,21 @@ function App() {
       </div>
       <Counter />
       <div className="android-link">
-        <a href="http://comboros-buvette.alwaysdata.net/apk">
+        <a
+          className="download-app-link"
+          href="http://comboros-buvette.alwaysdata.net/apk"
+        >
           Téléchargez l'application mobile !
+        </a>
+      </div>
+      <div className="android-link">
+        <a
+          className="apk-help"
+          href="https://www.frandroid.com/comment-faire/tutoriaux/184151_comment-installer-un-fichier-apk-sur-son-terminal-android"
+          target="_blank"
+          rel="noreferrer"
+        >
+          (Comment installer un fichier APK sur Android)
         </a>
       </div>
     </div>
